7a0c7ead799263a477d79cfa0b6d87f0fed77b6f
Olivia12345 <oliviaonu@outlook.com>
2023-10-16 14:39:18 -0700

    Updated test folder structure

--- src/test/java/com/spring/backend/backend2/java/Backend2ApplicationTests.java
+++ src/test/java/com/spring/backend/backend2/java/Backend2ApplicationTests.java
@@ -3,11 +3,13 @@ package com.spring.backend.backend2.java;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootTest
 @EnableMongoRepositories
 @CrossOrigin
+@TestPropertySource(locations = "application-test.properties")
 class Backend2ApplicationTests {
 
 	@Test
